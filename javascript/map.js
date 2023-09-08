8cffbe9646ed79fae09e36a1c5889921dc1d774c
Alvaro <pursuance@gmail.com>
2023-09-08 11:11:16 +0200

    Update javascript/map.js
    Co-authored-by: Clément Prod'homme <clementprdhomme@users.noreply.github.com>

--- javascript/map.js
+++ javascript/map.js
@@ -8,30 +8,24 @@ const map = new maplibregl.Map({
 map.addControl(new maplibregl.NavigationControl({ showCompass: false }));
 
 map.on('load', function() {
-  map.addSource('basemap-satellite', {
-    type: 'raster',
-    tiles: ['https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}'],
-    tileSize: 256
+  map.addLayer(
+    {
+      id: 'basemap-satellite',
+      type: 'raster',
+      source: 'basemap-satellite',
+      paint: {
+        'raster-opacity': 0,
+      },
   });
 
   map.addLayer(
     {
-      "id": "basemap-satellite",
-      "type": "raster",
-      "source": "basemap-satellite"
-  });
-
-  map.addSource('basemap-light', {
-    type: 'raster',
-    tiles: ['https://a.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}{r}.png'],
-    tileSize: 256
-  });
-
-  map.addLayer(
-    {
-      "id": "basemap-light",
-      "type": "raster",
-      "source": "basemap-light"
+      id: 'basemap-light',
+      type: 'raster',
+      source: 'basemap-light',
+      paint: {
+        'raster-opacity': 1,
+      },
   });
 
   const zoomInButton = document.querySelector('button.maplibregl-ctrl-zoom-in .maplibregl-ctrl-icon');
