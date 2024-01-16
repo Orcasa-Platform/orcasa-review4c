1bea6b846655f2c323af535ab6b6f31197d79d67
Alvaro Leal <pursuance@gmail.com>
2024-01-16 18:52:01 +0100

    Update methodology page and chart styles. Add y axis ticks

--- javascript/d3-methodology-chart.js
+++ javascript/d3-methodology-chart.js
@@ -2,7 +2,7 @@ window.createMethodologyChart = (data) => {
   const container = d3.select('#methodology-chart');
   const element = container.node();
   const CHART_HEIGHT = 455;
-  const margin = { top: 20, right: 20, bottom: 40, left: 20 };
+  const margin = { top: 20, right: 20, bottom: 40, left: 36 };
   const { publications, metaAnalysis } = data;
   const publicationsData = Object.keys(publications).map(key => ({ date: d3.timeParse("%Y")(key), value: publications[key] }));
   const metaAnalysisData = Object.keys(metaAnalysis).map(key => ({ date: d3.timeParse("%Y")(key), value: metaAnalysis[key] }));
@@ -23,10 +23,11 @@ window.createMethodologyChart = (data) => {
   const maxYear = Math.ceil(d3.max(publicationsData.concat(metaAnalysisData), d => d.date.getFullYear()) / 10) * 10;
 
 
+  const RANGE_PADDING = 12;
   // Add X axis
   const x = d3.scaleTime()
     .domain([new Date(minYear, 0, 1), new Date(maxYear, 0, 1)])
-    .range([ 0, width ]);
+    .range([ 0 + RANGE_PADDING, width - RANGE_PADDING ]);
 
   const xAxis = d3.axisBottom(x);
 
@@ -36,23 +37,49 @@ window.createMethodologyChart = (data) => {
     years.push(new Date(i, 0, 1)); // January 1st of the year
   }
   xAxis.tickValues(years)
-  svg.append("g")
-    .attr("transform", `translate(0,${height + 10})`)
-    .call(xAxis)
-    .select(".domain").remove() // Remove the bottom line
+  const xAxisTicks = svg.append("g")
+    .attr("transform", `translate(0,${height - 10})`)
+    .call(xAxis);
 
-    svg.selectAll(".tick text")
+  xAxisTicks.select(".domain").remove() // Remove the bottom line
+
+
+  xAxisTicks.selectAll(".tick text")
     .style("font-size", 12)
-    .style("fill", gray500)
-    .attr("dy", 16);
+    .style("fill", gray500);
 
-    svg.selectAll(".tick line")
-    .style("stroke", gray500);
+  xAxisTicks.selectAll(".tick line").remove();
 
   const VERTICAL_AXIS_PADDING = 24;
   const y = d3.scaleLinear()
-    .domain(d3.extent(publicationsData, d => d.value ))
+    .domain([0, Math.max(...publicationsData.map(d => d.value))])
     .range([ height - VERTICAL_AXIS_PADDING, 0 ]);
+
+  const TICK_DISTANCE = 500;
+  const numTicks = Math.ceil((y.domain()[1] - y.domain()[0]) / TICK_DISTANCE);
+  const tickValues = Array.from({length: numTicks}, (_, i) => {
+    return y.domain()[0] + i * TICK_DISTANCE;
+  });
+
+  const yAxis = d3.axisLeft(y)
+    .tickValues(tickValues)
+    .tickFormat(d3.format("d"))
+    .tickSizeInner(-width); // Make the inner ticks extend across the chart area
+
+
+  const yAxisTicks = svg.append("g")
+    .call(yAxis)
+
+  yAxisTicks.select(".domain").remove() // Remove the left line
+
+  yAxisTicks.selectAll(".tick text")
+    .attr("transform", "translate(-20,0)");
+
+  yAxisTicks.selectAll(".tick line")
+    .style("stroke", gray400);
+  yAxisTicks.selectAll(".tick:not(:first-child) line")
+    .style("stroke-dasharray", "2,10")
+    .style("stroke", gray400);
 
   // Add the publications line
   svg.append("path")
