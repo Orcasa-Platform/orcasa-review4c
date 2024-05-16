17f7d34dccdafda7dfd3a6641cd9ad13dfad8c6b
Alvaro Leal <pursuance@gmail.com>
2024-05-16 12:24:34 +0200

    Don't show in mobile

--- javascript/event-listeners.js
+++ javascript/event-listeners.js
@@ -112,7 +112,7 @@ window.addEventListener('load', function () {
   // BANNER PRACTICES
   const isBannerPracticesClosed = localStorage.getItem('BANNER_PRACTICES_CLOSED');
   if (!isBannerPracticesClosed) {
-    elements.bannerPractices.classList.remove('hidden');
+    elements.bannerPractices.classList.remove('lg:hidden');
   }
 
 
